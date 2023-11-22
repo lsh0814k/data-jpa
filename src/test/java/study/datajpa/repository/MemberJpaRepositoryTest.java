f0712a70168f6d47d38bbb5741929e3fbdf8b7a1
lee <lsh0814k@gmail.com>
2023-11-22 05:36:27 -0800

    spring data jpa study

---
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,16 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberJpaRepositoryTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void find() {
+    }
+}

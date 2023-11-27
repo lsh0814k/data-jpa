e313fd696e58b97c35ffdf19b7bfafb9c3232e47
lee <lsh0814k@gmail.com>
2023-11-26 18:04:05 -0800

    예제 도메인 모델

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,12 +1,14 @@
 package study.datajpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.ToString;
+
+import static jakarta.persistence.FetchType.*;
 
 @Entity
 @Getter
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     // 기본 생성자는 필수
@@ -16,9 +18,25 @@ public class Member {
         this.username = username;
     }
 
-    @Id @GeneratedValue
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    @Id @GeneratedValue @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
 }

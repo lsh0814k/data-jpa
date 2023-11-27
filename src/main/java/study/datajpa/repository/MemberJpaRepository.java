cd23771dde0ccd5bc925b46cf1d95e793523697c
lee <lsh0814k@gmail.com>
2023-11-26 18:27:54 -0800

    순수 JPA를 이용한 CRUD

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,9 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
     @PersistenceContext
@@ -15,7 +18,25 @@ public class MemberJpaRepository {
         return member;
     }
 
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(em.find(Member.class, id));
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
+    }
+
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
     }
 }

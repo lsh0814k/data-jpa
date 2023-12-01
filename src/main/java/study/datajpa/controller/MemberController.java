c3f36680268b74121dd09a63fb376d0809087e87
lee <lsh0814k@gmail.com>
2023-11-30 21:20:33 -0800

    페이징과 정렬

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -2,9 +2,13 @@ package study.datajpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -24,8 +28,21 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<Member> members(@PageableDefault(size=5, sort="username") Pageable pageable) {
+        return memberRepository.findAll(pageable);
+    }
+
+    @GetMapping("/dto/members")
+    public Page<MemberDto> membersDtos(@PageableDefault(size = 5, sort = "username") Pageable pageable) {
+        Page<Member> members = memberRepository.findAll(pageable);
+        return members.map(member -> new MemberDto(member.getId(), member.getUsername(), ""));
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("member1"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("member" + i));
+        }
     }
 }
